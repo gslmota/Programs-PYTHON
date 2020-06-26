3ad1283030a8472a5aad847f29f4a88052138f1c
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-06-25 22:17:36 -0300

    update

--- Exercícios/Mundo 3/ex066.py
+++ Exercícios/Mundo 3/ex066.py
@@ -1,2 +1,2 @@
 # Listagem de produtos com seus preços
-produtos = ('Pão', 1, 'Leite', 3, 'Queijo' 20, 'Rosca', 2, 'Picolé', 4, 'Refrigerante', 2.50, 'Pastel', 5)
+produtos = ('Pão', 1, 'Leite', 3, 'Queijo', 20, 'Rosca', 2, 'Picolé', 4, 'Refrigerante', 2.50, 'Pastel', 5)
