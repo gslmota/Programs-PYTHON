2e414c5e15a553817128a1aadd29c3a0ec993ddb
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-06-25 22:24:52 -0300

    update

--- Exercícios/Mundo 3/ex066.py
+++ Exercícios/Mundo 3/ex066.py
@@ -1,2 +1,9 @@
 # Listagem de produtos com seus preços
 produtos = ('Pão', 1, 'Leite', 3, 'Queijo', 20, 'Rosca', 2, 'Picolé', 4, 'Refrigerante', 2.50, 'Pastel', 5)
+print('LISTAGEM DE PREÇOS')
+for pos in range(0, len(produtos)):
+    if pos % 2 == 0:
+        print(f'{produtos[pos]:.<30}', end = '')
+    else:
+        print(f'{produtos[pos]:>7}')
+
