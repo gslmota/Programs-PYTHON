3a19afac6b55e041bd9e1cf027466beab7c55cac
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-06-26 22:10:31 -0300

    update

--- Exercícios/Mundo 3/ex068.py
+++ Exercícios/Mundo 3/ex068.py
@@ -1,8 +1,15 @@
 # exercicio de lista 
 lista = []
+menor = 0
+maior = 0
 for c in range(0, 5):
     lista.append(int(input(f'Digite um valor para a posição {c}: ')))
+    if c == 0:
+        maior = menor = lista[c]
+    else:
+        if lista[c] > maior:
+            maior = lista[c]
+        if lista[c] < menor:
+            menor = lista[c]
 print(lista)
-menor = min(lista)
-maior = max(lista)
 print(f'O maior é {maior} e o menor é {menor}')
