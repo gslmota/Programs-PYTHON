cab81da3baf854b6beeb5158f7cb9120ba9471ca
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-06-27 20:43:04 -0300

    update

--- Exercícios/Mundo 3/ex070.py
+++ Exercícios/Mundo 3/ex070.py
@@ -3,6 +3,12 @@ val = []
 cont = 0
 while cont < 5:
     n = int(input('Digite um número: '))
-    val.append(n)
+    if cont == 0 or n > val[-1]:
+        val.append(n)
+    else:
+        pos = 0
+        while pos < len(val):
+            if n <= val[pos]:
+                
     cont += 1
 print(val)
