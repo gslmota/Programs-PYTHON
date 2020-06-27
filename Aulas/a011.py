e4c5858dccacb9a85c861bef338fd8e8f77d997f
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-06-26 21:40:48 -0300

    update

--- Aulas/a011.py
+++ Aulas/a011.py
@@ -3,3 +3,17 @@ num = [1, 2, 3, 4, 5, 6]
 print(num)
 num [2] = 45
 print(num)
+num.append(7) # adiciona o 7
+num.sort() # ordena
+num.sort(reverse = True) # ordena em ordem decrescente
+num.insert(2, 1) # na posição 2 ele adiciona o valor 1
+num.pop() # remove o ultimo elemento
+num.pop(2) # remove o elemento da posição 2
+num.remove(4) # remove o 4 da lista, se houver mais de 1, ele remove apenas a primeira ocorrencia
+
+
+
+valores = [] # cria uma lista vazia
+
+ 
+
