aca6e440908d02cd0c3c7b16ec6fb5f46225677f
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-06-27 21:09:36 -0300

    update

--- Exercícios/Mundo 3/ex072.py
+++ Exercícios/Mundo 3/ex072.py
@@ -1,10 +1,10 @@
 # Adicionando valores em lista e criando outras listas
 valores = []
-c = 0
+pares = []
+impares = []
 while True:
     number = int(input('Digite um número: '))
     valores.append(number)
-    c += 1
     d = (str(input('Deseja Continuar? [s/n]'))).strip().upper()
     if d == 'N':
         break
