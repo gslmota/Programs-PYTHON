055febeb1e07fb51c0ab8ec5125444d4ccee7c65
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-06-28 21:36:01 -0300

    Update

--- Aulas/a012.py
+++ Aulas/a012.py
@@ -10,3 +10,9 @@ teste.append(40)
 galera = list()
 galera.append(teste)
 print(galera)
+
+
+galera = [['joao', 23], ['maria', 43], ['pedro', 23]]
+print(galera)
+for p in galera:
+    print(f'{p[0]} tem {p[1]} anos de idade!')
