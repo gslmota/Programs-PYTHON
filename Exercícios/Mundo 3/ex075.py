c1536ef1ea51fe5e518da3d74c9fbd28714f03d7
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-06-28 21:39:51 -0300

    Update

--- Exercícios/Mundo 3/ex075.py
+++ Exercícios/Mundo 3/ex075.py
@@ -1,1 +1,10 @@
 # Listas Compostas
+dado = list()
+galera = list()
+quant = 0
+while True:
+    dado.append(str(input('Nome: ')))
+    dado.append(float(input ('Peso: ')))
+    galera.append(dado[:])
+    dado.clear()
+    quant += 1
