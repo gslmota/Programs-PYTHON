5715211d914031c26cfadc7d9dd2b97fffc960e3
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-06-28 21:37:30 -0300

    Update

--- Aulas/a012.py
+++ Aulas/a012.py
@@ -16,3 +16,21 @@ galera = [['joao', 23], ['maria', 43], ['pedro', 23]]
 print(galera)
 for p in galera:
     print(f'{p[0]} tem {p[1]} anos de idade!')
+
+
+dado = list()
+galera = list()
+tmaior = tmenor = 0
+for c in range(0, 3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input ('Idade: ')))
+    galera.append(dado[:])
+    dado.clear()
+for p in galera:
+    if p[1] > 21:
+        print(f'{p[0]} é maior de idade!')
+        tmaior += 1
+    else:
+        print(f'{p[0]} é menor de idade!')
+        tmenor += 1
+print(f'Temos {tmaior} de idade e {tmenor} de idade!')
