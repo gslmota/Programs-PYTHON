c3baabd2c09d02eb3878cb329b66ae25c2d25068
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-06-29 20:47:29 -0300

    Update

--- Exercícios/Mundo 3/ex080.py
+++ Exercícios/Mundo 3/ex080.py
@@ -13,3 +13,9 @@ print(ficha)
 print(f'{"No.":< 4}{"Nome":< 10}')
 for i, a in enumerate(ficha):
     print(f'{i:< 4}{a[0]:< 10}{a[2]:> 8.1f}')
+while True:
+    opc = int(input('Mostrar notas de qual aluno? 0 para'))
+    if opc == 0:
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
