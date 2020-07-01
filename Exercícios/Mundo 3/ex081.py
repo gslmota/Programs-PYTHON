1cfb1dd939779cacd125a14493932a0eb4da5d65
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-06-30 21:58:46 -0300

    Update

--- Exercícios/Mundo 3/ex081.py
+++ Exercícios/Mundo 3/ex081.py
@@ -2,3 +2,12 @@
 aluno = {}
 nome = str(input('Digite o nome do aluno: '))
 media = float(input('Digite a media do aluno: '))
+nome.append(aluno)
+media.append(aluno)
+if media < 7:
+    situação = 'Reprovado'
+    situação.append(aluno)
+else: 
+    situação = 'Aprovado'
+    situação.append(aluno)
+print(aluno)
