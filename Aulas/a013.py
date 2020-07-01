b72d6dde2f263b8e2618ddbc799441d94c3b6e00
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-06-30 21:28:13 -0300

    Update

--- Aulas/a013.py
+++ Aulas/a013.py
@@ -16,3 +16,9 @@ del dados['nome']
 
 
 # Criando um dicionario dentro de uma lista
+brasil = []
+estado1 = {'uf' : 'Rio de Janeiro', 'sigla' : 'RJ'}
+estado2 = {'uf' : 'Minas Gerais', 'sigla' : 'MG'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(brasil)
