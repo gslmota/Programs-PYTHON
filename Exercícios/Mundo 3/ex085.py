5704a6bf4030be07a813ae7d6cdf0eda97597321
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-02 20:39:44 -0300

    Update

--- Exercícios/Mundo 3/ex085.py
+++ Exercícios/Mundo 3/ex085.py
@@ -1,1 +1,15 @@
 # Unindo dicionarios e listas
+pessoas = {}
+dadosPessoas = []
+tcadastrada = 0
+while True:
+    pessoas.clear()
+    pessoas['Nome'] = str(input('Digite o seu nome: ')).strip()
+    pessoas['Sexo'] = str(input('Digite o seu sexo: [M/F]')).strip().upper()
+    pessoas['Idade'] = int(input('Digite a sua idade: '))
+    dadosPessoas.append(pessoas.copy())
+    tcadastrada += 1
+    s = str(input('Deseja Continuar? [S/N]'))
+    if s in 'Nn':
+        break
+print(f'Foram cadastradas {tcadastrada} pessoas!')
