9858e756d972a6c3db479fd95c81deed8650469a
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-02 19:51:26 -0300

    Update

--- Exercícios/Mundo 3/ex084.py
+++ Exercícios/Mundo 3/ex084.py
@@ -1,5 +1,18 @@
 # Gerenciamento de jogador de futebol
 jogador = {
-    'Nome' : str(input('Nome: '))
+    'Nome' : str(input('Nome: ')), 
     'Partidas' : int(input('Quantidade de partidas: '))
 }
+tgols = []
+c = 0
+qgols = 0
+while c < jogador['Partidas']:
+    n = int(input(f'Qual o total de gols na partida {c+1}?'))
+    tgols.append(n)
+    qgols += n
+jogador['Gols'] = tgols[:]
+jogador['Total de Gols'] = qgols
+for k, v in jogador.items():
+    print(f'O campo {k} tem valor {v}!')
+
+
