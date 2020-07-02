42b44f04d856c070eb857cb3d588d8044ba0a005
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-02 20:43:01 -0300

    Update

--- Exercícios/Mundo 3/ex085.py
+++ Exercícios/Mundo 3/ex085.py
@@ -2,14 +2,18 @@
 pessoas = {}
 dadosPessoas = []
 tcadastrada = 0
+soma = media = 0
 while True:
     pessoas.clear()
     pessoas['Nome'] = str(input('Digite o seu nome: ')).strip()
     pessoas['Sexo'] = str(input('Digite o seu sexo: [M/F]')).strip().upper()
     pessoas['Idade'] = int(input('Digite a sua idade: '))
+    soma += pessoas['Idade']
     dadosPessoas.append(pessoas.copy())
     tcadastrada += 1
     s = str(input('Deseja Continuar? [S/N]'))
     if s in 'Nn':
         break
 print(f'Foram cadastradas {tcadastrada} pessoas!')
+media = soma / len(dadosPessoas)
+print(f'A media de idade é {media} anos!')
