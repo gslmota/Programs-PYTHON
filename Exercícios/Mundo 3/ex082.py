3954ff6d45075435d7a071dd16357b538d360cb6
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-01 21:34:51 -0300

    Update

--- Exercícios/Mundo 3/ex082.py
+++ Exercícios/Mundo 3/ex082.py
@@ -1,3 +1,11 @@
 # Jogo de 4 pessoas usando o dado!
 from random import randint
 from time import sleep
+jogadas = {}
+jogadas['jogador 1'] = randint(1,6)
+jogadas['jogador 2'] = randint(1,6)
+jogadas['jogador 3'] = randint(1,6)
+jogadas['jogador 4'] = randint(1,6)
+for k, v in jogadas.items():
+    sleep(1)
+    print(f'O {k} jogou {v}!')
