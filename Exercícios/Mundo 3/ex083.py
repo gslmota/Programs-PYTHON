51946702247eeae5fb6d46949713bad76684ab1c
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-01 22:07:59 -0300

    Update

--- Exercícios/Mundo 3/ex083.py
+++ Exercícios/Mundo 3/ex083.py
@@ -1,1 +1,9 @@
 # Cadastro de trabalhador 
+from datetime import date
+cadastro = dict()
+cadastro['nome'] = str(input('Nome: '))
+cadastro['nascimento'] = int(input('Data de Nascimento: '))
+idade = date.year - cadastro['nascimento']
+cadastro['idade'] = idade
+if cadastro['idade'] >= 35:
+    cadastro['aposentadoria'] = 'Você pode se aposentar!'
