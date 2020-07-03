dbe36a39f64b9cded38dee7e8e589fe883baaed5
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-02 21:18:34 -0300

    Update

--- Exercícios/Mundo 3/ex086.py
+++ Exercícios/Mundo 3/ex086.py
@@ -3,9 +3,22 @@ time = list()
 jogador = dict()
 partidas = list()
 while True: 
+    jogador.clear()
     jogador['nome'] = str(input('Digite o nome: '))
     tot = int(input(f'Quantas partidas {jogador["nome"]} jogou?'))
+    partidas.clear()
     for c in range(0,tot):
         partidas.append(int(input(f'Quantod gols na partida {c+1} ?')))
     jogador['gols'] = partidas[:]
     jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    r = str(input('Quer continuar ?[S/N]'))
+    if r in 'Nn':
+        break
+print('-' * 40)
+for k, v in enumerate(time):
+    print(f'{k:>3}', end ='')
+    for d in k.values():
+        print(f'{str(d):<15}', end = '')
+    print()
+print('-' * 40)
