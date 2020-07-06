4f926b8fdc04ef0406876643b782de8fa1ca09b2
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-05 21:30:40 -0300

    Updarte

--- Aulas/a014.py
+++ Aulas/a014.py
@@ -1,4 +1,6 @@
 # Aula de Funções
+
+valores =[2, 3, 5, 6, 89]
 def linha():
     print('-' * 30)
 
@@ -9,6 +11,11 @@ def mensagem(msg):
     linha()
 
 
+
+def dobra(valores):
+    for c in valores:
+        valores[c] = 2 * valores[c]
+    print(valores)
 
 def soma(a,b):
     s = a + b
@@ -28,3 +35,4 @@ soma(2,54)
 contador(1, 3)
 contador(1, 3, 4, 5, 6)
 contador(3)
+dobra(valores)
