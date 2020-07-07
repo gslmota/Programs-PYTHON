17f8c2adaced19ddd2e7db87dd00bfa20a0cca04
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-06 21:04:21 -0300

    Update

--- Exercícios/Mundo 3/ex089.py
+++ Exercícios/Mundo 3/ex089.py
@@ -1,1 +1,10 @@
 # Função que realiza contagem
+def contador(i, f, p):
+    if i > f:
+        for x in range(i, f, p):
+            print(x)
+    else:
+        for x in range(i, f, -p):
+            print(x)
+
+# programa principal
