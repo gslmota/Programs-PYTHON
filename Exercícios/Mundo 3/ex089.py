41fe0b31abc33cc3460f7c0b319b290798777288
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-06 21:12:43 -0300

    Update

--- Exercícios/Mundo 3/ex089.py
+++ Exercícios/Mundo 3/ex089.py
@@ -1,10 +1,19 @@
+from time import sleep
 # Função que realiza contagem
 def contador(i, f, p):
-    if i > f:
+    if f > i:
         for x in range(i, f, p):
             print(x)
+            sleep(0.5)
     else:
         for x in range(i, f, -p):
             print(x)
+            sleep(0.5)
+
+
 
 # programa principal
+ini = int(input('Digite o inicio da contagem: '))
+fim = int(input('Digite o final da contagem: '))
+pas = int(input('Digite o passo da contagem: '))
+contador(ini, fim, pas) 
