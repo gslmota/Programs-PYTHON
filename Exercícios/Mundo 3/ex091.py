16cccc90e63985bcd72f55297039aef90aa730b0
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-08 20:28:57 -0300

    Update

--- Exercícios/Mundo 3/ex091.py
+++ Exercícios/Mundo 3/ex091.py
@@ -2,13 +2,13 @@
 from random import randint
 from time import sleep
 def sortearLista(lista):
-    print('Sorteando 5 valores para a lista!', end ='')
+    print('Sorteando 5 valores para a lista: ', end ='')
     for cont in range(0, 5):
         n = randint(1, 10)
         lista.append(n)
         print(f'{n}', end = '', flush = True)
         sleep(0.3)
-    print('Pronto!!!')
+    print(' Pronto!!!')
 
 def somaPares(lista):
     soma = 0
@@ -16,9 +16,6 @@ def somaPares(lista):
         if val % 2 == 0:
             soma += val
     print(f'Somando os valores pares de {lista}, temos {soma}')   
-
-
-
 
 # Programa principal
 num = list()
