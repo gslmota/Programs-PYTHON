cc08c2763f5c67d3ad46d60cb93b7269e2daa42b
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-08 20:20:37 -0300

    Update

--- Exercícios/Mundo 3/ex091.py
+++ Exercícios/Mundo 3/ex091.py
@@ -1,8 +1,13 @@
 # Sortear números e somar
 from random import randint
+from time import sleep
 def sortearLista(lista):
     for cont in range(0, 5):
-        lista.append(randint(1, 10))
+        n = randint(1, 10)
+        lista.append(n)
+        print(f'{n}', end = '', flush = True)
+        sleep(0.3)
+    print('Pronto!!!')
 
 def somaPares():
 
