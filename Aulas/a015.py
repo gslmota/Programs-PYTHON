ed157e5a0b8816e5193f4d4917b83427b8f125a6
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-09 20:49:13 -0300

    Update

--- Aulas/a015.py
+++ Aulas/a015.py
@@ -3,4 +3,15 @@
 # interactive help
  # Função help que ajuda em qualquer coisa -> help () ou help(nome do comando ex : print)
  # help(input) é a mesma coisa que print(input.__doc__)
- 
+
+ # docstrings
+def contador(i, f, p):
+    c = i
+    while c <= f:
+        print(f'{c}', end = '')
+        c += p
+    print('Fim!')
+
+
+# Programa principal
+contador(2, 20, 5)
