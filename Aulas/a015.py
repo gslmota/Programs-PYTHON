1665fdfda23806c58618b232caf43f31b7d6c8f4
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-09 20:43:00 -0300

    Update

--- Aulas/a015.py
+++ Aulas/a015.py
@@ -1,4 +1,6 @@
 # Contiuação da aula de funções
 
 # interactive help
- # Função help que ajuda em qualquer coisa -> help ()
+ # Função help que ajuda em qualquer coisa -> help () ou help(nome do comando ex : print)
+ # help(input) é a mesma coisa que print(input.__doc__)
+ 
