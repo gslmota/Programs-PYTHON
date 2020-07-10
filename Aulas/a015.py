08e29e49b138c6ddc5bce6b97cf4829ebdb9a799
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-09 21:21:49 -0300

    Update

--- Aulas/a015.py
+++ Aulas/a015.py
@@ -48,3 +48,10 @@ def soma(a = 0, b = 0):
 # programa principal
 resposta = soma(2,6)
 print(resposta)
+
+
+def fatorial(num = 1):
+    f =1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
