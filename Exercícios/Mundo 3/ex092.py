c8d632b53f790172818a8a8f375d1b28b5f91dee
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-13 21:47:15 -0300

    Update

--- Exercícios/Mundo 3/ex092.py
+++ Exercícios/Mundo 3/ex092.py
@@ -6,11 +6,11 @@ def voto(ano):
     atual = date.today().year
     idade = atual - ano
     if idade < 16:
-        return f'Com {idade} não vota!'
+        return f'Com {idade} anos, não vota!'
     elif idade < 18:
-        return f'Com {idade} o voto é opcionla!'
+        return f'Com {idade} anos, o voto é opcionla!'
     else: 
-        return f'Com {idade} o voto é obrigatório!'
+        return f'Com {idade}anos, o voto é obrigatório!'
 
 
 # Programa principal
