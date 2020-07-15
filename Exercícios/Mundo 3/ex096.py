51765c4ec582ace2a0ab42bfc1689e9bd2231c41
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-14 21:38:31 -0300

    U´pdate

--- Exercícios/Mundo 3/ex096.py
+++ Exercícios/Mundo 3/ex096.py
@@ -5,8 +5,16 @@ def notas(*n, sit = False):
     r['maior'] = max(n)
     r['menor'] = min(n)
     r['media'] = sum(n) / len(n)
-    
-    
+    if sit:
+        if r['media'] >= 7:
+            r['situacao'] = 'Boa'
+        elif r['media'] >= 5:
+            r['situacao'] = 'Razoavel'
+        else:
+            r['situacao'] = 'Ruim'
+    return r
+
+
 # Programa principal
-resp = notas(5.5, 3.4, 7, 4.9)
+resp = notas(5.5, 3.4, 7, 4.9, sit = True)
 print(resp)
