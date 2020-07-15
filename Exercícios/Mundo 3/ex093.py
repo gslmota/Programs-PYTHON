d3f25803fafbb172a9a2bbdb658b82c0db16ec16
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-14 21:00:43 -0300

    Fim

--- Exercícios/Mundo 3/ex093.py
+++ Exercícios/Mundo 3/ex093.py
@@ -2,7 +2,15 @@
 def fatorial(num, show = False):
     f = 1
     for c in range(num, 0, -1):
+        if show:
+            print(c, end = '')
+            if c > 1:
+                print(' x ', end = '')
+            else:
+                print(' = ', end = '')
         f *= c
     return f
 
 
+# Programa principal
+print(fatorial(5, show = True))
