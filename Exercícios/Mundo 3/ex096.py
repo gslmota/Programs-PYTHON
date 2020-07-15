bb1a63961c3a6a106002c1829a5501c405ce23f7
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-14 21:33:01 -0300

    Update

--- Exercícios/Mundo 3/ex096.py
+++ Exercícios/Mundo 3/ex096.py
@@ -1,4 +1,12 @@
 # Analisando dicionarios
+def notas(*n, sit = False):
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['media'] = sum(n) / len(n)
+    
+    
 # Programa principal
 resp = notas(5.5, 3.4, 7, 4.9)
 print(resp)
