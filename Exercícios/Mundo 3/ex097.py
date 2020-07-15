cdf4cb03391cc1132cc8d05f8c1f32b8febfe05b
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-14 22:07:03 -0300

    Fim

--- Exercícios/Mundo 3/ex097.py
+++ Exercícios/Mundo 3/ex097.py
@@ -1,20 +1,29 @@
 # Sistema de ajuda em Python
+from time import sleep
+cores = ('\033[m', '\033[0;30;41m', '\033[0;30;42m', '\033[0;30;43m', '\033[0;30;44m', '\033[0;30;45m', '\033[7;30m')
 def ajuda(com):
+    título(f'Acessando o manula do comando \'{com}\'', 4)
+    print(cores[6], end = '')
     help(com)
-
+    print(cores[0], end = '')
+    sleep(2)
 
 def título(msg, cor = 0):
-    tam = len(msg)
+    tam = len(msg) + 4
+    print(cores[cor], end = '')
     print('-' * tam)
-    print(msg)
+    print(f'  {msg}')
     print('-' * tam)
+    print(cores[0], end = '')
+    sleep(1)
+
 #Programa Principal
 comando = ''
 while True:
-    print('Sistema de Ajuda!')
+    título('Sistema de Ajuda!', 2)
     comando = str(input("Função ou Biblioteca: "))
     if comando.upper() == 'FIM':
         break
     else:
         ajuda(comando)
-        título('Até Logo!')
+título('Até Logo!', 1)
