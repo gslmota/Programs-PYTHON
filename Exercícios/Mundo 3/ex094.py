ada53c0f02c9c73e6cc524ef38db3d1f3599f63a
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-14 21:13:42 -0300

    Fim

--- Exercícios/Mundo 3/ex094.py
+++ Exercícios/Mundo 3/ex094.py
@@ -1,1 +1,17 @@
 # Ficha de jogador
+def ficha(nome = '<desconhecido>', gols = 0):
+    print(f'O jogador {nome} fez {gols} no campeonato!')
+
+
+# Programa principal
+nome = str(input('Nome: '))
+gols = str(input('Gols: '))
+if gols.isnumeric():
+    gols = int(gols)
+else: 
+    gols = 0
+
+if nome.strip() == '':
+    ficha(gol = gols)
+else:
+    ficha(nome, gols)
