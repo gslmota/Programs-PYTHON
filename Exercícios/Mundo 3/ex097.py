4a06d5bb839d55df91391c161ff109a3d4ab1d84
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-14 22:08:04 -0300

    Fim

--- Exercícios/Mundo 3/ex097.py
+++ Exercícios/Mundo 3/ex097.py
@@ -2,7 +2,7 @@
 from time import sleep
 cores = ('\033[m', '\033[0;30;41m', '\033[0;30;42m', '\033[0;30;43m', '\033[0;30;44m', '\033[0;30;45m', '\033[7;30m')
 def ajuda(com):
-    título(f'Acessando o manula do comando \'{com}\'', 4)
+    título(f'Acessando o manual do comando \'{com}\'', 4)
     print(cores[6], end = '')
     help(com)
     print(cores[0], end = '')
