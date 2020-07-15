801576d68113c2a45a501df6589312aa63d1d9cc
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-15 20:56:48 -0300

    Update

--- Aulas/a016.py
+++ Aulas/a016.py
@@ -1,4 +1,9 @@
 # Aula de Modularização
+def fatorial(numero):
+    f = 1
+    for c in range(1, numero + 1):
+        
 # Programa principal
 num =  int(input("Digite um numero: "))
 fat = fatorial(num)
+print(f'O fatorial de {num} é {fat}!')
