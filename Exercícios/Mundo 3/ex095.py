8ce9e869fa8e968595c70dda8cca0c49ed44d0e5
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-14 21:22:49 -0300

    U´pdate

--- Exercícios/Mundo 3/ex095.py
+++ Exercícios/Mundo 3/ex095.py
@@ -1,1 +1,19 @@
 # valida numero
+def LeiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else :
+            print('\033[0;31m Erro! Digite um número Válido!\033[m')
+        if ok:
+            break
+    return valor
+
+
+# Programa Principal
+
+            
