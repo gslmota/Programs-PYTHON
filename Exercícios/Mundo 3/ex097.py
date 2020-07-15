b504e0162d77144b3d1fcad7059774dad084409f
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-14 21:44:21 -0300

    Update

--- Exercícios/Mundo 3/ex097.py
+++ Exercícios/Mundo 3/ex097.py
@@ -1,1 +1,11 @@
 # Sistema de ajuda em Python
+def ajuda(com):
+    
+#Programa Principal
+comando = ''
+while True:
+    comando = str(input("Função ou Biblioteca: "))
+    if comandox.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando)
