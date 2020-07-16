aef4b3c6c118c49111d15cf9b9b73f24a5b2e56b
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-15 21:01:31 -0300

    Update

--- Aulas/a016.py
+++ Aulas/a016.py
@@ -16,3 +16,5 @@ def triplo(n):
 num =  int(input("Digite um numero: "))
 fat = fatorial(num)
 print(f'O fatorial de {num} é {fat}!')
+print(f'O dobro de {num} é {dobro(num)}!')
+print(f'O triplo de {num} é {triplo(num)}!')
