2eec1e7694b1c7955d24b57c6591cab819b8a085
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-15 21:09:45 -0300

    Update

--- Aulas/a016.py
+++ Aulas/a016.py
@@ -1,20 +1,9 @@
 # Aula de Modularização
-def fatorial(numero):
-    f = 1
-    for c in range(1, numero + 1):
-        f *= c
-    return f
-
-
-def dobro(n):
-    return n * 2
-
-
-def triplo(n):
-    return n * 3
+# Arquivo de Modulos : Uteis
+import uteis
 # Programa principal
 num =  int(input("Digite um numero: "))
-fat = fatorial(num)
+fat = uteis.fatorial(num)
 print(f'O fatorial de {num} é {fat}!')
-print(f'O dobro de {num} é {dobro(num)}!')
-print(f'O triplo de {num} é {triplo(num)}!')
+print(f'O dobro de {num} é {uteis.dobro(num)}!')
+print(f'O triplo de {num} é {uteis.triplo(num)}!')
