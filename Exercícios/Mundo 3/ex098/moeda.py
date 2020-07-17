e36492b88a442a013c75dd56f8d437fb44108b86
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-16 21:21:23 -0300

    uP

--- Exercícios/Mundo 3/ex098/moeda.py
+++ Exercícios/Mundo 3/ex098/moeda.py
@@ -1,8 +1,13 @@
 # Estudo de Modularização
 def aumentar(preco, taxa):
-    return 
+    res = preco + (preco * taxa)/100
+    return res
+
 
 def diminuir(preco, taxa):
+    res = preco - (preco * taxa)/100
+    return res
+    
 
 def dobro(preco):
 
