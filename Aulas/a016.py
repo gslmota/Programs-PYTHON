595c7bbfca47288658be0e6ef0fc39aa5b7337e3
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-16 21:37:33 -0300

    up

--- Aulas/a016.py
+++ Aulas/a016.py
@@ -1,10 +1,12 @@
 # Aula de Modularização
 # Voce tera mais organização, facil manutenção, ocultar codigo, reutilização
 # Arquivo de Modulos : Uteis
-import uteis
+import Aulas.uteis.funcao
+
 # Programa principal
 num =  int(input("Digite um numero: "))
-fat = uteis.fatorial(num)
+fat = Aulas.uteis.fatorial(num)
 print(f'O fatorial de {num} é {fat}!')
-print(f'O dobro de {num} é {uteis.dobro(num)}!')
-print(f'O triplo de {num} é {uteis.triplo(num)}!')
+print(f'O dobro de {num} é {Aulas.uteis.dobro(num)}!')
+print(f'O triplo de {num} é {Aulas.uteis.triplo(num)}!')
+print(f'A soma de 2 e 3 é {Aulas.uteis.funcao.soma(2, 3)}')
---
+++ Aulas/uteis/funcao/__init__.py
@@ -0,0 +1,3 @@
+def soma(n1, n2):
+    soma = n1 + n2
+    return soma
