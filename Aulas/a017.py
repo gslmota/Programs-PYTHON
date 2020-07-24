1ce9d67a2af5065dff94d0b64a4a6c69da5062a9
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-23 21:10:31 -0300

    Up

--- Aulas/a017.py
+++ Aulas/a017.py
@@ -4,6 +4,8 @@ try:
     a = int(input("Digite um num: "))
     b = int(input('Digite um num'))
     c = a/b
+except ValueError:
+    print('problema nos dados')
 except Exception as erro:
     print(f'Nao funcionou{erro.__class__}')
 else:
