5f0935ac5babfaa171c89fef9cd19bc478cdb0e2
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-24 19:40:33 -0300

    Função leia int

--- Exercícios/Mundo 3/ex098.py
+++ Exercícios/Mundo 3/ex098.py
@@ -1,1 +1,13 @@
 # funções aprofudadas em python
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('ERRO! Digite um número válido!')
+            continue
+        else:
+            return n
+
+
+# Programa principal
