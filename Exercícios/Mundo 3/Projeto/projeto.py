9d2e3fa5c8080cce65b9bb9d3077263363372a0c
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-26 19:15:04 -0300

    up

--- Exercícios/Mundo 3/Projeto/projeto.py
+++ Exercícios/Mundo 3/Projeto/projeto.py
@@ -1,3 +1,4 @@
 from lib.interface import *
 cabecalho('Sistema Arquivo v1.0')
-menu(['opc01', 'opc02', 'opc03'])
+print()
+resposta = menu(['Criar Arquivo', 'Cadastrar Pessoas', 'Listar Pessoas', 'Sair do Sistema'])
