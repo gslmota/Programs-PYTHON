d8764cea48d71f02e381cf4492ada27bfd7cfb0c
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-26 19:37:29 -0300

    update

--- Exercícios/Mundo 3/Projeto/projeto.py
+++ Exercícios/Mundo 3/Projeto/projeto.py
@@ -1,4 +1,15 @@
 from lib.interface import *
+from time import sleep
 cabecalho('Sistema Arquivo v1.0')
-print()
-resposta = menu(['Criar Arquivo', 'Cadastrar Pessoas', 'Listar Pessoas', 'Sair do Sistema'])
+while  True:
+    resposta = menu(['Cadastrar Pessoas', 'Listar Pessoas', 'Sair do Sistema'])
+    if resposta == 1:
+        cabecalho('Opção 1')
+    elif resposta == 2:
+        cabecalho('Opção 2')
+    elif resposta == 3:
+        cabecalho('Saindo do Sistema!')
+        break
+    else:
+        print('\033[31m ERRO: Por favor digite uma opção válida.\033[m')
+        sleep(1)
