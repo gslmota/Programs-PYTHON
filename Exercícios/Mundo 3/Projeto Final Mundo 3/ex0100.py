19a89f9999f330ad460532e71849f26daa28307d
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-26 18:42:47 -0300

    up

--- Exercícios/Mundo 3/Projeto Final Mundo 3/ex0100.py
+++ Exercícios/Mundo 3/Projeto Final Mundo 3/ex0100.py
@@ -1,5 +1,5 @@
 # programa par ao projeto de python
 # projeto final para uso de arquivos
 # projeto usando importações, usando cores, usando funçoes, usando laços de repetição
-# parte 1
-# escreva seu codigo aqui--->>>
+
+
