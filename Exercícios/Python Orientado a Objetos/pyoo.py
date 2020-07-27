4711a248ea7ade23351ee415e0accc75e0243ef5
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-27 20:37:43 -0300

    update

--- Exercícios/Python Orientado a Objetos/pyoo.py
+++ Exercícios/Python Orientado a Objetos/pyoo.py
@@ -5,8 +5,13 @@ class Computador:
         self.memoria = nmemoria
         self.placa = nplaca
     pass
+    def ligar(self):
+        print('Ligando')
+    
+    def 
 
 computador1 = Computador('Acer', '8gb', 'AMD')
 computador2 = Computador('Asus', '16g', 'NVIDIA')
 print(computador1.marca, computador1.memoria, computador1.placa)
 print(computador2.marca, computador2.memoria, computador2.placa)
+computador1.ligar()
