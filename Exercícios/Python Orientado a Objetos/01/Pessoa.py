88aa643dcd736d1201a718dd5100d6074bc6e3f6
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-29 20:37:06 -0300

    up

--- Exercícios/Python Orientado a Objetos/01/Pessoa.py
+++ Exercícios/Python Orientado a Objetos/01/Pessoa.py
@@ -48,3 +48,9 @@ class Pessoa:
 
     def get_ano_nascimento(self):
         return self.ano_atual - self.idade
+
+
+    @classmethod
+    def por_ano_nascimento(cls, nome,ano_nascimento):
+        idade = cls.ano_atual - ano_nascimento
+        return
