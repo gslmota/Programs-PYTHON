7891277e63a6f6fd5760836c76b2b32b76f993f1
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-29 16:45:35 -0300

    update

--- Exercícios/Python Orientado a Objetos/Main.py
+++ Exercícios/Python Orientado a Objetos/Main.py
@@ -2,5 +2,4 @@ from Pessoa import Pessoa
 p1 = Pessoa('Gabriel', 19)
 p2 = Pessoa('maria',13)
 p1.comer('uva')
-# Parei aqui
-# video 19:36
+p1.comer('uva')
--- Exercícios/Python Orientado a Objetos/Pessoa.py
+++ Exercícios/Python Orientado a Objetos/Pessoa.py
@@ -1,11 +1,15 @@
 class Pessoa:
-   def __init__(self, nome, idade, comendo = False, falando = False):
+    def __init__(self, nome, idade, comendo = False, falando = False):
        self.nome = nome
        self.idade = idade
        self.comendo = comendo
        self.falando = falando
 
-   def comer(self, alimento):
+    def comer(self, alimento):
+        if self.comendo:
+            print(f'{self.nome} já está comendo!')
+            return
+        
         print(f'{self.nome} está comendo {alimento}!')
         self.comendo = True
 
