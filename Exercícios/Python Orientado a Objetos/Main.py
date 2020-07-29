2dd0def2a9eb9fa1c970d4de6f3227eed08bc0c6
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-29 16:58:42 -0300

    update

--- Exercícios/Python Orientado a Objetos/Main.py
+++ Exercícios/Python Orientado a Objetos/Main.py
@@ -2,5 +2,8 @@ from Pessoa import Pessoa
 p1 = Pessoa('Gabriel', 19)
 p2 = Pessoa('maria',13)
 p1.comer('uva')
+p1.falar('POO')
 p1.parar_de_comer()
+p1.falar('POO')
+p1.parar_falar()
 p1.comer('uva')
--- Exercícios/Python Orientado a Objetos/Pessoa.py
+++ Exercícios/Python Orientado a Objetos/Pessoa.py
@@ -5,6 +5,24 @@ class Pessoa:
        self.comendo = comendo
        self.falando = falando
 
+
+    def falar(self, assunto):
+        if self.comendo:
+            print(f'{self.nome}não pode falar comendo!')
+            return
+        if self.falando:
+            print(f'{self.nome} já está falando sobre {assunto}!')
+            self.falando = True
+
+
+    def parar_falar(self):
+        if not self.falando:
+            print(f'{self.nome} não está falando!')
+            return
+        print(f'{self.nome} parou de falar.')
+        self.falando = False
+
+        
     def comer(self, alimento):
         if self.comendo:
             print(f'{self.nome} já está comendo!')
