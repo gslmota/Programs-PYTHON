d951f45ef202444ebc877b336e81da180e67416b
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-07-31 20:40:24 -0300

    fim

--- Exercícios/Python Orientado a Objetos/02/Main.py
+++ Exercícios/Python Orientado a Objetos/02/Main.py
@@ -1,1 +1,5 @@
-import Produto
+from Produto import Produto
+# classe main
+p1 = Produto('Camiseta', 50)
+p1.desconto(10)
+print(p1.name, p1.preco)
--- Exercícios/Python Orientado a Objetos/02/Produto.py
+++ Exercícios/Python Orientado a Objetos/02/Produto.py
@@ -35,7 +35,3 @@ class Produto:
 
 
 
-# classe main
-p1 = Produto('Camiseta', 50)
-p1.desconto(10)
-print(p1.preco)
