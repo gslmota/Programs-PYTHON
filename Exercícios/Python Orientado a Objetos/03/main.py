dc26a98aad91923670436578d942c0f3acac5f08
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-08-01 21:01:48 -0300

    up

--- Exercícios/Python Orientado a Objetos/03/main.py
+++ Exercícios/Python Orientado a Objetos/03/main.py
@@ -2,8 +2,10 @@ from a import A
 # Programa principal
 a1 = A()
 a2 = A()
-
+print(A.var)
 # Alterando valor direto na classe
 A.var = 000
+# novo valor 
+print(A.var)
 print(a1.var)
-print(a2.var)
+print(a2.var)   
