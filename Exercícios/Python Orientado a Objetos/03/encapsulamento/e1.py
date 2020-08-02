df8c4a85a9d03e0e0af8d4ee2d61787edf5d0443
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-08-02 18:44:44 -0300

    update

--- Exercícios/Python Orientado a Objetos/03/encapsulamento/e1.py
+++ Exercícios/Python Orientado a Objetos/03/encapsulamento/e1.py
@@ -8,4 +8,14 @@ class BasedeDados:
             self.dados['clientes'] = {id: nome}
         else:
             self.dados['clientes'].update({id: nome})
-            
+
+
+    def listaClientes(self):
+        for id, nome in self.dados['clientes'].items():
+            print(id, nome)
+
+
+    def apagaClientes(self, id):
+        del self.dados['clientes'][id]
+
+        
