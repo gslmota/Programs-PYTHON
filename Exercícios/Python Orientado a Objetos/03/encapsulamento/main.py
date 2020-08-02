7f538da39b1a7197b0c6b03427622eff5d1db334
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-08-02 18:45:49 -0300

    up

--- Exercícios/Python Orientado a Objetos/03/encapsulamento/main.py
+++ Exercícios/Python Orientado a Objetos/03/encapsulamento/main.py
@@ -2,4 +2,7 @@ from e1 import BasedeDados
 bd = BasedeDados()
 bd.inserirClientes(1, 'gabriel')
 bd.inserirClientes(4, 'ilda')
+bd.inserirClientes(2, 'jorge')
 print(bd.dados)
+bd.apagaClientes(2)
+bd.listaClientes()
