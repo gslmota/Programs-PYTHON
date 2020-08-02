d80c403d2fd3ccd61a58191149702666f13dc1fe
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-08-02 18:38:29 -0300

    update

--- Exercícios/Python Orientado a Objetos/03/encapsulamento/e1.py
+++ Exercícios/Python Orientado a Objetos/03/encapsulamento/e1.py
@@ -3,4 +3,9 @@ class BasedeDados:
     def __init__(self):
         self.dados = {}
 
-    
+    def inserirClientes(self, id, nome):
+        if 'clientes' not in self.dados:
+            self.dados['clientes'] = {id: nome}
+        else:
+            self.dados['clientes'].update({id: nome})
+            
