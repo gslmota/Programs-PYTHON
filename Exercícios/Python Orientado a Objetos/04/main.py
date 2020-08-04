8b8639590fc48cf882f19140201504d8e31a1b30
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-08-04 20:25:15 -0300

    up

--- Exercícios/Python Orientado a Objetos/04/main.py
+++ Exercícios/Python Orientado a Objetos/04/main.py
@@ -1,6 +1,8 @@
 from cl import Escritor
+from cl import Maquina
 from cl import Caneta
 escritor = Escritor('Joao')
 caneta = Caneta('Bic')
+maquina = Maquina()
 print(escritor.nome)
 print(caneta.marca)
