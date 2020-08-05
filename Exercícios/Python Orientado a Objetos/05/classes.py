b5192967f92bdb709a054d3e5aff915cb22b896a
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-08-05 20:56:19 -0300

    up

--- Exercícios/Python Orientado a Objetos/05/classes.py
+++ Exercícios/Python Orientado a Objetos/05/classes.py
@@ -10,6 +10,15 @@ class CarrinhoDeCompras:
     def listaProdutos(self):
         for produto in self.produtos:
             print(produto.nome, produto.valor)
+
+    
+    def somaTotal(self):
+        soma = 0
+        for produto in self.produtos:
+            soma += produto.valor
+        return soma
+
+        
 class Produto:
     def __init__(self, nome, valor):
         self.nome = nome
