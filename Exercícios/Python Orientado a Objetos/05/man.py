22ff4176cbce66dfcacd3760e8e1e3bc2e32a09e
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-08-05 21:00:33 -0300

    up

--- Exercícios/Python Orientado a Objetos/05/man.py
+++ Exercícios/Python Orientado a Objetos/05/man.py
@@ -1,3 +1,5 @@
 from classes import CarrinhoDeCompras, Produto
 carrinho = CarrinhoDeCompras()
-produto = Produto()
+produto1 = Produto('Camiseta', 50)
+produto2 = Produto('Sapato', 150)
+produto3 = Produto('Calça', 180)
