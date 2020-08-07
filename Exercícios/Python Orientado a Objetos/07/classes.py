960a66873edef8f07d2a821590dcb25ae7ed9bfd
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-08-07 20:11:01 -0300

    Update classes.py

--- Exercícios/Python Orientado a Objetos/07/classes.py
+++ Exercícios/Python Orientado a Objetos/07/classes.py
@@ -1,3 +1,9 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+        
 class Cliente:
     def __init__(self, nome, idade):
         self.nome = nome
